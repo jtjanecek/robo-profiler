9c6466eec2f6148cd2c58eb38965ff520735badf
John Janecek <janecektyler@gmail.com>
2021-12-31 20:13:19 -0700

    refactor

--- deframing/deframe_profile.py
+++ deframing/deframe_profile.py
@@ -1,59 +1,70 @@
 import sys
 sys.path.append('/home/fourbolt/Documents/uya/robo/src')
 
+import numpy as np
+import time
 import json
+import datetime
 
 from utils import utils
 
 data = {}
+def read_json(server):
+    with open(f'../logs/{server}.json', 'r') as f:
+        loaded = json.loads(f.read())
+    for i in range(len(loaded)):
+        loaded[i][0] = utils.hex_to_bytes(loaded[i][0])
+        for j in range(len(loaded[i][1])):
+            loaded[i][1][j] = utils.hex_to_bytes(loaded[i][1][j])
+    data[server] = loaded
 
-for server in ['mls', 'dmetcp']:
-
-    with open(f'{server}_deframe_data.json', 'r') as f:
-        mls_data = json.loads(f.read())
-
-    mls_data_clean = []
-
-    for i in range(len(mls_data)):
-        mls_data[i][0][1] = utils.hex_to_bytes(mls_data[i][0][1])
-        i_data = mls_data[i][0][1]
-
-        d_data = []
-        for j in range(len(mls_data[i][1])):
-            d = utils.hex_to_bytes(mls_data[i][1][j][1])
-            d_data.append(d)
-        if d_data != []:
-            mls_data_clean.append([i_data, d_data])
-    data[server] = mls_data_clean
+read_json('mls')
+read_json('dmetcp')
 
 def deframe_accuracy(deframe_class):
     print("Checking accuracy ...")
 
     for server, packets in data.items():
         deframer = deframe_class()
-        print(f" -- Checking {server} ...")
+        print(f" -- Checking {server} ...", end='   ')
         for input_data, results in packets:
-            deframed = deframer.deframe([input_data])
+            deframed = deframer.deframe(input_data)
             if deframed != results:
+                print("\n")
                 print("Incorrect match.")
                 print(f" Input: {input_data}")
                 print(f" Got: {deframed}")
                 print(f" Expected: {results}")  
                 raise AssertionError()
+        print("Done")
 
 def deframe_profile(deframe_class):
     for server, packets in data.items():
         deframer = deframe_class()
         for input_data, results in packets:
-            deframed = deframer.deframe([input_data])
+            deframed = deframer.deframe(input_data)
+
+'''
+for input_data, results in data['dmetcp']:
+    for result in results:
+        if result not in input_data:
+            print(input_data, results)
+            import sys; sys.exit()
 
 
+'''
 if __name__ == '__main__':
     from utils.rtbufferdeframer import RtBufferDeframer
 
     deframe_accuracy(RtBufferDeframer)
 
+    print(f"{datetime.datetime.now()} | Running profiling ...")
+    init_time = time.time()
     import timeit
     t = timeit.Timer('deframe_profile(RtBufferDeframer)', setup='from __main__ import deframe_profile; from utils.rtbufferdeframer import RtBufferDeframer')
-    print(t.repeat(repeat=20, number=100))
+    total_time = t.repeat(repeat=20, number=40)
+    print("Profiled time:",total_time)
+    print(f"Mean: {np.mean(total_time)}")
+    print("Real time (min):", (time.time()-init_time) / 60)
+
 
---
+++ logs/extract.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+inputs = defaultdict(list)
+
+
+def bytes_to_hex(data:bytes):
+    return data.hex().upper()
+
+def hex_to_bytes(hex:str):
+    return bytes(bytearray.fromhex(hex))
+
+with open('main.log','r') as f:
+    for line in f:
+        server = line.split("robo.")[-1].split()[0]
+        if server in ['dmeudp','nat']:
+            continue
+        if '| I |' in line:
+            inputs[server].append(['i',line.split("| I |")[-1].strip()])
+        elif 'Deframe |' in line:
+            inputs[server].append(['d',line.split("| ")[-1].strip()])
+   
+
+pairs = defaultdict(list)
+
+for server in inputs.keys():
+    while len(inputs[server]) != 0:
+        i = inputs[server].pop(0)    
+        d = []
+        while len(inputs[server]) > 0 and inputs[server][0][0] != 'i':
+
+            d.append(inputs[server].pop(0))
+
+        pairs[server].append([i,d])
+
+import json
+
+data = {}
+for server in ['mls', 'dmetcp']:
+    
+    mls_data = pairs[server]
+
+    mls_data_clean = []
+
+    for i in range(len(mls_data)):
+        i_data = mls_data[i][0][1]
+
+        d_data = []
+        for j in range(len(mls_data[i][1])):
+            d = mls_data[i][1][j][1]
+            d_data.append(d)
+        if d_data != []:
+            mls_data_clean.append([i_data, d_data])
+
+    with open(f'{server}.json', 'w') as f:
+        f.write(json.dumps(mls_data_clean, indent=4))
+    
+
